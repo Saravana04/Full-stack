c2f9baa5776a2415eb5f74d71e77850e4928aec5
Saravana Balaji <saravanabalaji04@gmail.com>
2018-10-23 20:45:36 +0530

    Final push

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,57 +1,41 @@
-//$(document).ready(function (){
-
-var Data = 
-    {
-    "git": [
-        {
-            "name": "Saravana",
-            "repositories": [
-                "Git_Demo",
-                "Angular_code",
-                "Css_files",
-                "scripts"
-            ]
-        },
-        {
-            "name": "Balaji",
-            "repositories": [
-                "ver 1",
-                "ver 2"
-            ]
-        }
-    ]
-    };
-    var tr=$('<tr/>');
-//});
+var tr=$('<tr/>');
+var count = 0;
 function fetch(){
     var usr = document.getElementById('uname').value;
     console.log(usr);
-    for(var i = 0; i < Data.git.length; i++)
-    {   
-        var User = Data.git[i];
-        if(User.name === usr){
-            //tr = $('<tr/>');
-            
-            for(var j = 0; j < User.repositories.length; j++)
-            {
-              var repo = User.repositories[j];
-              
-              tr.append( "<tr><td>" + repo + "</td>"+ "<td>" +"<button onclick=redirect()>"+"New issue</button>" + "</td>" + "</tr>");              
+    count = count+1;
+        $.ajax({
+            url: "https://api.myjson.com/bins/93dfw",
+            dataType: "json",
+            success: function(data){
+                console.log(data);
+
+                //$(data.git).each(function(index,value){
+                for(var i = 0; i < data.git.length; i++)
+                    {      
+                    var User = data.git[i]    
+                    //var User = value.name;
+                    User.issues = count;
+                    if(User.name === usr){
+                        for(var j = 0; j < User.repositories.length; j++)
+                        {
+                            var repo = User.repositories[j];
+                            
+                            tr.append( "<tr><td>" + repo + "</td>"+ "<td>" +"<button onclick=redirect()>"+"New issue</button>" + "</td>" + "<td>"+User.issues +"</td></tr>");              
+                        }
+                    }   
+                    $('table').append(tr);  
+                //});
             }
-            
-            //$('table').append(tr);
-            
-        }
+        } 
+        });    
         
+        
+          
     }
-    $('table').append(tr);     
-}
+
 
 function redirect(){
     window.open('/Full-Stack/Sample/issue.html');
 }
 
-function issue_count(){
-
-
-}
--- scripts/script2.js
+++ scripts/script2.js
@@ -5,8 +5,18 @@ function testJS() {
     count= count+1;
     console.log(count);
 
+    $.ajax({
+        url: 'https://api.myjson.com/bins/93dfw',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            alert("Success");
+        },
+        data: JSON.stringify(count)
+    });
     //var b = document.getElementById('name').value,
-        url = '/Full-Stack/Sample/index.html?count=' + encodeURIComponent(count);
+        //url = '/Full-Stack/Sample/index.html?count=' + encodeURIComponent(count);     
 
-    window.open(url);
+    window.open('/Full-Stack/Sample/index.html');
 }
